4b8b740b51079cc6230fde8a84ab61ebba0e62eb
lopsig <jonathan.ls.trabajo@gmail.com>
2025-09-23 19:40:31 -0500

    actualizacion de Const API_URL

--- src/App/pages/CreateBrewPage.jsx
+++ src/App/pages/CreateBrewPage.jsx
@@ -1,102 +1,3 @@
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
-
-
-// export const CreateBrewPage = () => {
-//   const navigate = useNavigate();
-
-//   const createBrew = async (e) => {
-
-//     e.preventDefault();
-//     const formData = new FormData();
-//     const name = e.target.name.value;
-//     const style = e.target.style.value;
-//     const abv = e.target.abv.value;
-//     const srm = e.target.srm.value;
-//     const ibu = e.target.ibu.value;
-//     const ml = e.target.ml.value;
-//     const price = e.target.price.value;
-//     const description = e.target.description.value;
-//     const image = e.target.image.files[0];
-//     const id_user = localStorage.getItem('id_user');
-
-
-//     formData.append('name', name);
-//     formData.append('style', style);
-//     formData.append('abv', abv);
-//     formData.append('srm', srm);
-//     formData.append('ibu', ibu);
-//     formData.append('ml', ml);
-//     formData.append('price', price);
-//     formData.append('description', description);
-//     formData.append('image', image);
-//     formData.append('id_user', id_user);
-//     try {
-
-//       const response = await axios.post('http://localhost:8000/brews/create_brew', formData, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data'
-//         }
-//       });
-//       console.log(response.data.message);
-//       navigate('/be/home-provider');
-//     }catch (error) {
-//       console.error("Error creating brew:", error.response.data);
-//       const errorMessage = error.response.data.detail || 'Error desconocido al crear la cerveza.';
-
-//       alert(errorMessage);
-//     }
-
-//   }
-
-
-//   return (
-//     <div>
-//       <h1>Registrar nueva cerveza</h1>
-//       <form onSubmit={(e) => createBrew(e)}>
-//         <div>
-//           <label>Nombre de Cerveza:</label>
-//           <input type="text" name="name" placeholder="Nombre de Cerveza" required/>
-//         </div>
-//         <div>
-//           <label>Estilo:</label>
-//           <input type="text" name="style" placeholder="Estilo de Cerveza" required />
-//         </div>
-//         <div>
-//           <label>ABV:</label>
-//           <input type="number" step="0.1" name="abv" placeholder="Grado Alcohólico" required />
-//         </div>
-//         <div>
-//           <label>SRM:</label>
-//           <input type="number" step="0.1" name="srm" placeholder="Referencia de Color" required />
-//         </div>
-//         <div>
-//           <label>IBU:</label>
-//           <input type="number" step="0.1" name="ibu" placeholder="Amargor" required />
-//         </div>
-//         <div>
-//           <label>ml:</label>
-//           <input type="number" name="ml" placeholder="Mililitros" required />
-//         </div>
-//         <div>
-//           <label>Precio:</label>
-//           <input type="number" step="0.01" name="price" placeholder="Precio" required />
-//         </div>
-//         <div>
-//           <label>Descripción:</label>
-//           <textarea name="description" placeholder="Descripción de la Cerveza" required />
-//         </div>
-//         <div>
-//           <label>Imagen:</label>
-//           <input type="file" name="image" required />
-//         </div>
-//         <button type="submit">Registrar Cerveza</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
 import { Card } from 'primereact/card';
@@ -109,7 +10,8 @@ import { FileUpload } from 'primereact/fileupload';
 import { useNavigate } from "react-router-dom";
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import logo from '/src/assets/img/logoSF.png'; // Asegúrate de que este logo sea blanco o claro para el fondo oscuro
+import logo from '/src/assets/img/logoSF.png'; 
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const CreateBrewPage = () => {
   const navigate = useNavigate();
@@ -233,7 +135,7 @@ export const CreateBrewPage = () => {
       const id_user = sessionStorage.getItem('id_user');
       submitData.append('id_user', id_user);
 
-      const response = await axios.post('http://localhost:8000/brews/create_brew', submitData, {
+      const response = await axios.post(`${API_URL}/brews/create_brew`, submitData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
--- src/auth/pages/RegisterBreweryPage.jsx
+++ src/auth/pages/RegisterBreweryPage.jsx
@@ -1,100 +1,3 @@
-// import axios from "axios"
-// import { useNavigate } from "react-router-dom"
-
-// export const RegisterBreweryPage = () => {
-//   const navigate = useNavigate();
-//   const handleRegisterBrewery = async (event) => {
-//     event.preventDefault();
-//     const formData = new FormData(event.target);
-//     const data = Object.fromEntries(formData.entries());
-
-//     try {
-
-//       const response = await axios.post('http://localhost:8000/breweries/register_brewery', data, {
-//         headers: {
-//           'Content-Type': 'multipart/form-data'
-//         }
-//       });
-//       console.log(response.data.message);
-//       navigate("/auth/login");
-//     } catch (error) {
-//       console.error("Error registering brewery:", error.response.data);
-//       const errorMessage = error.response.data.detail || 'Error desconocido al registrar la cervecería.';
-
-//       alert(errorMessage);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Registrate como Proveedor</h1>
-//       <form onSubmit={handleRegisterBrewery}>
-//         <div>
-//           <label>Nombre de Cervecería:</label>
-//           <input type="text" name="name_brewery" placeholder="Nombre de Cervecería" required />
-//         </div>
-//         <div>
-//           <label>RUC:</label>
-//           <input type="number" name="ruc" placeholder="RUC" required />
-//         </div>
-//         <div>
-//           <label>Razón Social:</label>
-//           <input type="text" name="name_comercial" placeholder="Razón Social" required />
-//         </div>
-//         <div>
-//           <label>Ciudad:</label>
-//           <input type="text" name="city" placeholder="Ciudad" required />
-//         </div>
-//         <div>
-//           <label>Dirección:</label>
-//           <input type="text" name="address" placeholder="Dirección" required />
-//         </div>
-//         <div>
-//           <label>Número de Contacto:</label>
-//           <input type="number" name="contact_number" placeholder="Número de Contacto" required />
-//         </div>
-//         <div>
-//           <label>Horario de Atención:</label>
-//           <input type="text" name="opening_hours" placeholder="Horario de Atención" required />
-//         </div>
-//         <div>
-//           <label>Descripción:</label>
-//           <textarea name="description" placeholder="Descripción" required />
-//         </div>
-//         <div>
-//           <label>Logo:</label>
-//           <input type="file" name="logo" required />
-//         </div>
-//         <div>
-//           <label>Correo:</label>
-//           <input type="email" name="email" placeholder="Correo" required />
-//         </div>
-//         <div>
-//           <label>Contraseña:</label>
-//           <input type="password" name="password" placeholder="Contraseña" required />
-//         </div>
-//         <div>
-//           <label>Confirmar Contraseña:</label>
-//           <input type="password" placeholder="Confirmar Contraseña" required />
-//         </div>
-
-//         <div>
-//         <button type="submit">Registar</button>
-//         </div>
-
-
-//         <button onClick={() => navigate("/auth/register_prov")}>Registar Proveedor</button>
-//       </form>
-//     </div>
-//   );
-// }
-
-
-
-
-
-
-
 import axios from "axios";
 import { useState, useEffect, useRef } from "react"; // <-- Importa useRef
 import { Card } from 'primereact/card';
@@ -110,6 +13,7 @@ import { useNavigate } from "react-router-dom";
 import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import logo from '/src/assets/img/logoSF.png';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const RegisterBreweryPage = () => {
   const navigate = useNavigate();
@@ -272,7 +176,7 @@ export const RegisterBreweryPage = () => {
         }
       });
 
-      const response = await axios.post('http://localhost:8000/breweries/register_brewery', submitData, {
+      const response = await axios.post(`${API_URL}/breweries/register_brewery`, submitData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
